3a591a9d4112bd1a5ba378909d4a9fc51878be47
Pacino125 <borovskypatrik@gmail.com>
2024-05-15 12:09:25 +0200

    #saveable fields in fishActivity and new textfields

--- app/src/main/java/com/example/myapplication/FishActivity.kt
+++ app/src/main/java/com/example/myapplication/FishActivity.kt
@@ -3,8 +3,15 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,10 +21,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.data.FishType
 import com.example.myapplication.database.FishingLicenseDbContext
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -29,7 +43,7 @@ class FishActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val dbContext = FishingLicenseDbContext(this)
-        fishTypes = dbContext.getAllFishTypes()
+        fishTypes = dbContext.getAllFishTypes().sortedBy { fishType: FishType -> fishType.type}
 
         setContent {
             MyApplicationTheme {
@@ -44,39 +58,91 @@ class FishActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FishScreen(fishTypes: List<FishType>) {
-    val selectedFishType = remember { mutableStateOf(fishTypes[0]) }
-    val expanded = remember { mutableStateOf(false) }
+    val selectedFishTypeId = rememberSaveable { mutableStateOf(fishTypes[0].guid) }
+    var selectedFishType by remember { mutableStateOf(fishTypes.find { it.guid == selectedFishTypeId.value } ?: fishTypes[0]) }
+    val expanded = rememberSaveable { mutableStateOf(false) }
+    val countValue = rememberSaveable { mutableStateOf("") }
+    val lengthValue = rememberSaveable { mutableStateOf("") }
+    val weightValue = rememberSaveable { mutableStateOf("") }
 
-    Box(
+    Column(
         modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+            .fillMaxSize()
+            .padding(horizontal = 32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ExposedDropdownMenuBox(
-            expanded = expanded.value,
-            onExpandedChange = { expanded.value = !expanded.value}
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
         ) {
-            TextField(
-                value = selectedFishType.value.type,
-                onValueChange = {},
-                readOnly = true,
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded.value) },
-                modifier = Modifier.menuAnchor()
-            )
-            ExposedDropdownMenu(
+            ExposedDropdownMenuBox(
                 expanded = expanded.value,
-                onDismissRequest = { expanded.value = false }
+                onExpandedChange = { expanded.value = !expanded.value }
             ) {
-                fishTypes.forEach {
-                    DropdownMenuItem(
-                        text = { Text(text = it.type) },
-                        onClick = { 
-                            selectedFishType.value = it
-                            expanded.value = false
-                        }
-                    )
+                TextField(
+                    value = TextFieldValue(selectedFishType.type),
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded.value) },
+                    modifier = Modifier.menuAnchor()
+                )
+                ExposedDropdownMenu(
+                    expanded = expanded.value,
+                    onDismissRequest = { expanded.value = false }
+                ) {
+                    fishTypes.forEach {
+                        DropdownMenuItem(
+                            text = { Text(text = it.type) },
+                            onClick = {
+                                selectedFishTypeId.value = it.guid
+                                selectedFishType = it
+                                expanded.value = false
+                            }
+                        )
+                    }
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = countValue.value,
+            onValueChange = { countValue.value = it },
+            label = { Text(stringResource(R.string.fish_count)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+            ),
+            singleLine = true,
+            enabled = selectedFishType.type in listOf(stringResource(R.string.fish_another_type),
+                stringResource(
+                    R.string.fish_white_fish
+                )
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = lengthValue.value,
+            onValueChange = { lengthValue.value = it },
+            label = { Text(stringResource(R.string.fish_length)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+            ),
+            singleLine = true,
+            enabled = selectedFishType.type !in listOf(stringResource(R.string.fish_another_type),
+                stringResource(
+                    R.string.fish_white_fish
+                )
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = weightValue.value,
+            onValueChange = { weightValue.value = it },
+            label = { Text(stringResource(R.string.fish_weight)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+            ),
+            singleLine = true
+        )
     }
 }
